af177298ec34dcc48c8484b83298b57c3ea8ef3c
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-30 02:16:37 +0300

    Update LogEvent test is enhanced

--- tests/pkg/helper/r_logger_test.go
+++ tests/pkg/helper/r_logger_test.go
@@ -241,4 +241,209 @@ func TestReleaseLoggerWithFx(t *testing.T) {
 			assert.Contains(t, _msg, msg)
 		})
 	})
+
+	t.Run("r-logger=LogEvent", func(t *testing.T) {
+		t.Parallel()
+
+		var g fx.DotGraph
+		var l *helper.RLogger
+
+		app := fxtest.New(
+			t,
+			fx.Logger(fxtest.NewTestPrinter(t)),
+			fx.WithLogger(func() fxevent.Logger { return fxtest.NewTestLogger(t) }),
+			helper.ReleaseModule,
+			fx.Populate(&g),
+			fx.Populate(&l),
+			pkg.FakeModule,
+		).RequireStart()
+
+		defer app.RequireStop()
+
+		require.NoError(t, app.Err())
+		assert.Contains(t, g, `"fx.DotGraph" [label=<fx.DotGraph>];`)
+
+		t.Run("r-logger=LogEvent-OnStartExecuting-nil", func(t *testing.T) {
+			event := fxevent.OnStartExecuting{}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuting-f-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuting{
+				FunctionName: "test-f",
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuting-c-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuting{
+				CallerName: "test-c",
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuting", func(t *testing.T) {
+			event := fxevent.OnStartExecuting{
+				FunctionName: "test-f",
+				CallerName:   "test-c",
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-error-nil", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				Err: fmt.Errorf("test-e"),
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-error-f-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				FunctionName: "test-f",
+				Err:          fmt.Errorf("test-e"),
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-error-c-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				CallerName: "test-c",
+				Err:        fmt.Errorf("test-e"),
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-error-m-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				Method: "test-m",
+				Err:    fmt.Errorf("test-e"),
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-no-error-f-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				FunctionName: "test-f",
+				Err:          nil,
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-no-error-c-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				CallerName: "test-c",
+				Err:        nil,
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-no-error-m-name", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				Method: "test-m",
+				Err:    nil,
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStartExecuted-no-error", func(t *testing.T) {
+			event := fxevent.OnStartExecuted{
+				FunctionName: "test-f",
+				CallerName:   "test-c",
+				Method:       "test-m",
+				Err:          nil,
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStopExecuting", func(t *testing.T) {
+			event := fxevent.OnStopExecuting{}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStopExecuting-f-name", func(t *testing.T) {
+			event := fxevent.OnStopExecuting{
+				FunctionName: "test-f",
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStopExecuting-c-name", func(t *testing.T) {
+			event := fxevent.OnStopExecuting{
+				CallerName: "test-c",
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStopExecuting", func(t *testing.T) {
+			event := fxevent.OnStopExecuting{
+				FunctionName: "test-f",
+				CallerName:   "test-c",
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStopExecuting", func(t *testing.T) {
+			event := fxevent.OnStopExecuting{
+				FunctionName: "test-f",
+				CallerName:   "test-c",
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStopExecuted-error", func(t *testing.T) {
+			event := fxevent.OnStopExecuted{
+				FunctionName: "test-f",
+				CallerName:   "test-c",
+				Err:          fmt.Errorf("test-e"),
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-OnStopExecuted-no-error", func(t *testing.T) {
+			event := fxevent.OnStopExecuted{
+				FunctionName: "test-f",
+				CallerName:   "test-c",
+				Err:          nil,
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-Supplied-error", func(t *testing.T) {
+			event := fxevent.Supplied{
+				TypeName:   "test-t",
+				ModuleName: "test-m",
+				Err:        fmt.Errorf("test-e"),
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-Supplied-no-error", func(t *testing.T) {
+			event := fxevent.Supplied{
+				TypeName:   "test-t",
+				ModuleName: "test-m",
+				Err:        nil,
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-Decorated-error", func(t *testing.T) {
+			event := fxevent.Decorated{
+				DecoratorName:   "test-d",
+				ModuleName:      "test-m",
+				OutputTypeNames: []string{"test-o"},
+				Err:             nil,
+			}
+			l.LogEvent(&event)
+		})
+
+		t.Run("r-logger=LogEvent-Decorated-error-no", func(t *testing.T) {
+			event := fxevent.Decorated{
+				DecoratorName:   "test-d",
+				ModuleName:      "test-m",
+				OutputTypeNames: []string{"test-o"},
+				Err:             nil,
+			}
+			l.LogEvent(&event)
+		})
+	})
 }
