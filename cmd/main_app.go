70c8c8d94b40cd29859fe00b0a2f8e3830c8b66a
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-09-01 17:14:27 +0300

    Add main app added for only test purposes to do integration test

---
+++ cmd/main_app.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/kaankoken/helper/pkg"
+	"github.com/kaankoken/helper/pkg/helper"
+	"go.uber.org/fx"
+)
+
+func MainApp() *fx.App {
+	app := fx.New(
+		pkg.FakeTagModule,
+		pkg.FakeFlavorDevModule,
+		helper.LoggerModule,
+		fx.Invoke(RegisterHooks),
+	)
+
+	return app
+}
+
+// RegisterHooks -> Registering lifecycle of fx & running http server (Gin)
+func RegisterHooks(lifecycle fx.Lifecycle, logger *helper.LogHandler) {
+	lifecycle.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+				logger.Info("Starting application")
+				return nil
+			},
+			OnStop: func(context.Context) error {
+				logger.Info("Stopping application")
+				return nil
+			},
+		},
+	)
+}
